4a491e0761e274eb0a634812825d58e745f2fb85
devfab17 <u202220219@upc.edu.pe>
2024-11-13 23:52:41 -0500

    all

--- src/app/public/pages/profile/profile.component.ts
+++ src/app/public/pages/profile/profile.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from "@angular/core";
-import {NgForOf} from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import {NgForOf, NgIf} from "@angular/common";
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'; // Importamos DomSanitizer
 import {
   MatCard,
   MatCardActions,
@@ -8,7 +9,7 @@ import {
   MatCardSubtitle,
   MatCardTitle
 } from "@angular/material/card";
-import {MatButton} from "@angular/material/button";
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-profile',
@@ -21,7 +22,8 @@ import {MatButton} from "@angular/material/button";
     MatCardTitle,
     MatCardHeader,
     MatCard,
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
@@ -38,31 +40,53 @@ export class ProfileComponent implements OnInit {
       {
         username: 'JavaMuse🪼',
         date: 'Dec 10, 2022',
-        message: 'Recomienden sus mejores animes, por favor'
+        message: 'Recomienden sus mejores animes, por favor',
+        media: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/14/db/41/caption.jpg?w=1200&h=-1&s=1'  // Imagen
       },
       {
         username: 'Sergio Kun Aguero',
         date: 'Dec 9, 2022',
-        message: 'Cuando miran a tu chica 🤣'
+        message: 'Cuando miran a tu chica 🤣',
+        media: ''  // Sin media
       },
       {
         username: 'Shotta❄️',
         date: 'Sep 15, 2022',
-        message: 'Oda n’a plus le temps de rigoler et il est déjà entrain de le prouver #ONEPIECE1060'
+        message: 'Oda n’a plus le temps de rigoler et il est déjà entrain de le prouver #ONEPIECE1060',
+        media: 'https://www.youtube.com/watch?v=XtpIBGnA-a4'  // Video de YouTube
       },
       {
         username: 'Ibai',
         date: 'Mar 29, 2022',
-        message: 'Lo necesito 🥺'
+        message: 'Lo necesito 🥺',
+        media: ''  // Sin media
+      },
+      {
+        username: 'MikuLover',
+        date: 'Jan 20, 2023',
+        message: '¿Alguien más ve anime en la madrugada?',
+        media: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/14/db/41/caption.jpg?w=1200&h=-1&s=1'  // Imagen
+      },
+      {
+        username: 'TechGuru',
+        date: 'Feb 5, 2023',
+        message: 'Nuevo tutorial de desarrollo web',
+        media: 'https://www.youtube.com/watch?v=XtpIBGnA-a4'  // Video de YouTube
       }
     ]
   };
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {}
 
+  // Método para sanitizar el URL de YouTube
+  getSafeVideoUrl(url: string): SafeResourceUrl {
+    const videoId = url.split('v=')[1].split('&')[0];  // Extrae el ID del video
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${videoId}`);
+  }
+
   navigateToTweets() {
-    // Navigate to the user's tweets page (implement routing logic as needed)
+    // Navegar a la página de tweets del usuario (implementa la lógica de enrutamiento si es necesario)
   }
 }
